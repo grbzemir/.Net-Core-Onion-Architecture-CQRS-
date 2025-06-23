65c3ff958c24dae13ee7e1ed9d0b083b5b950c77
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 00:19:54 +0300

    Product

--- src/Core/ProductApp.Domain/Entities/Product.cs
+++ src/Core/ProductApp.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductApp.Domain.Commen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace ProductApp.Domain.Entities
 {
-    internal class Product
+    public class Product:BaseEntity
     {
+        public String Name { get; set; }
+        public decimal Value { get; set; }
+        public int Quantity { get; set; }
     }
 }
