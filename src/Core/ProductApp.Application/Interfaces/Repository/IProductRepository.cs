2a1b7d1ddbfbd2d509c11161b76d2bdd376f3bb3
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 00:21:28 +0300

    Interfaces

---
+++ src/Core/ProductApp.Application/Interfaces/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Domain.Entities;
+
+namespace ProductApp.Application.Interfaces.Repository
+{
+    public interface IProductRepository:IGenericRepositoryAsync<Product>
+    {
+       
+    }
+}
