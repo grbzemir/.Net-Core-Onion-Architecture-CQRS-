33c1d8ba3ceaf2932f53d9e6b67894fef33b28b7
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 00:21:17 +0300

    AllRepository

---
+++ src/Core/ProductApp.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using ProductApp.Domain.Commen;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Interfaces.Repository
+{
+    public interface IGenericRepositoryAsync<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid Id);
+        Task<T> AddAsync(T entity);
+        //Task<T> UpdateAsync(T entity);
+        //Task<bool> DeleteAsync(Guid Id);
+
+    }
+}
