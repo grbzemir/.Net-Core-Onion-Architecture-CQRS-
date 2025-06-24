5637f695b7ab41a322b7b7b1d70b2004c77890a6
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 17:41:17 +0300

    Controllers

---
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Features.Queries.GetAllProducts;
+using ProductApp.Application.Interfaces.Repository;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetA()
+        {
+            var query = new GetAllProductsQuery();
+
+            return Ok(await _mediator.Send(query));
+        }
+
+
+    }
+}
