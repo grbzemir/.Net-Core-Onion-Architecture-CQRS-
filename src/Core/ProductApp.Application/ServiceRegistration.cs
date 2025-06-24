16e41f43bbeaa657f5fde6684a17475e74fc051a
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 17:40:51 +0300

    Services

---
+++ src/Core/ProductApp.Application/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+using AutoMapper;
+
+namespace ProductApp.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationRegistration(this IServiceCollection services)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+
+            services.AddAutoMapper(assm);
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assm)); // ✅
+
+
+        }
+    }
+}
