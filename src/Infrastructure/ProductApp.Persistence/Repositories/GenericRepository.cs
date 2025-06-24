e491680a415e86f189992b88a03012757d2e7827
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 17:40:59 +0300

    Repositories

---
+++ src/Infrastructure/ProductApp.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Commen;
+using ProductApp.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity; 
+        }
+
+        public async Task<bool> DeleteAsync(Guid Id)
+        {
+            var entity = await _context.Set<T>().FindAsync(Id);
+            if (entity == null)
+            {
+                return false; 
+            }
+
+            _context.Set<T>().Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            
+            return await _context.Set<T>().ToListAsync();
+            
+        }
+
+        public async Task<T> GetByIdAsync(Guid Id)
+        {
+            return await _context.Set<T>().FindAsync(Id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
