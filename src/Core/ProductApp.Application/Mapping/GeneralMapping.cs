d501a9a90b635f9182eef29aa312bcd618e36df9
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 19:11:32 +0300

    AuttoMapper

--- src/Core/ProductApp.Application/Mapping/GeneralMapping.cs
+++ src/Core/ProductApp.Application/Mapping/GeneralMapping.cs
@@ -1,5 +1,9 @@
 ﻿using AutoMapper;
 using ProductApp.Application.Dto;
+using ProductApp.Application.Features.Commands.CreateProduct;
+using ProductApp.Application.Features.Commands.DeleteProduct;
+using ProductApp.Application.Features.Commands.UpdateProduct;
+using ProductApp.Application.Features.Queries.GetProductById;
 using ProductApp.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -15,6 +19,21 @@ namespace ProductApp.Application.Mapping
         {
             CreateMap<Product, ProductViewDto>();
             CreateMap<ProductViewDto, Product>();
+
+            CreateMap<Product, CreateProductCommand>();
+            CreateMap<CreateProductCommand, Product>();
+
+            CreateMap<Product, GetProductByIdViewModel>();
+            CreateMap<GetProductByIdViewModel, Product>();
+
+            CreateMap<Product, UpdateProductViewModel>();
+            CreateMap<UpdateProductViewModel, Product>();
+
+            CreateMap<Product, UpdateProductCommand>();
+            CreateMap<UpdateProductCommand, Product>();
+
+            CreateMap<Product, DeleteProductCommand>();
+            CreateMap<DeleteProductCommand, Product>();
         }
     }
 }
