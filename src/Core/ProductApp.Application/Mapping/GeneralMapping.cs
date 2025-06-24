989122407bb22ddddb258b556602a46c0145b076
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-24 17:40:42 +0300

    AutoMapper

---
+++ src/Core/ProductApp.Application/Mapping/GeneralMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductApp.Application.Dto;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Product, ProductViewDto>();
+            CreateMap<ProductViewDto, Product>();
+        }
+    }
+}
